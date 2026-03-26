38c75a827e3abf45e8221f709b4400e674d407ee
Yigitcan <kayhabyigit@gmail.com>
2026-03-26 22:18:28 +0300

    first commit

--- game3.js
+++ game3.js
@@ -1,54 +1,48 @@
-function test() {
+async function test() {
   console.log("REQUEST ATILIYOR");
-  GM_xmlhttpRequest({
-    method: "POST",
-    url: "https://axymxyglob.netlify.app/.netlify/functions/checkKey",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    data: JSON.stringify({ key: "ABC123" }),
 
-    onload: function(res) {
-              console.log("RESPONSE GELDİ:", res.responseText);
-      try {
-        const data = JSON.parse(res.responseText);
+  try {
+    const res = await fetch(
+      "https://axymxyglob.netlify.app/.netlify/functions/checkKey",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ key: "ABC123" }),
+      },
+    );
 
-        if (data.valid) {
-               let code = data.code.trim();
-          code = code.replace(/\\n/g, "\n");
+    const text = await res.text();
+    console.log("RESPONSE GELDİ:", text);
 
-    //runCode(data.code)
-          /*  const v = new Function(code);
-            v(code);*/
-          eval(code);
-        } else {
-          console.log("Geçersiz key");
-        }
-      } catch (e) {
-        console.log("Parse hatası:", res.responseText);
+    try {
+      const data = JSON.parse(text);
+
+      if (data.valid) {
+        let code = data.code.trim();
+        code = code.replace(/\\n/g, "\n");
+
+        runCode(code);
+      } else {
+        console.log("Geçersiz key");
       }
-    },
-
-    onerror: function(err) {
-      console.log("REQUEST HATA:", err);
+    } catch (e) {
+      console.error("Parse hatası:", text);
     }
-  });
+  } catch (err) {
+    console.error("REQUEST HATA:", err);
+  }
 }
 
 function runCode(code) {
-  const script = document.createElement("script");
-  script.textContent = code;
-  document.documentElement.appendChild(script);
-
-  // 💥 DOMContentLoaded kaçtıysa manuel tetikle
-/* code = code.replace(
-  /document\.addEventListener\("DOMContentLoaded",\s*\(.*?\)\s*=>\s*{([\s\S]*?)}\);/,
-  "$1"
-);*/
-
-  //script.remove();
+  try {
+    const script = document.createElement("script");
+    script.textContent = code;
+    document.documentElement.appendChild(script);
+  } catch (err) {
+    console.error("Kod çalıştırma hatası:", err);
+  }
 }
 
-window.addEventListener("load", () => {
-  test();
-});
+window.addEventListener("load", test);
